3d2d423de44c3cad3970c2d31d1e9b1c79413c66
IshikawaRryusuke <50456775+IshikawaRryusuke@users.noreply.github.com>
2019-09-30 23:06:38 +0900

    Update init.php

--- keizi/init.php
+++ keizi/init.php
@@ -8,18 +8,15 @@ $db['dbname'] = "board_test";  // データベース名
  
 <?php
  
-/* DSN組み立て */
 $dsn = sprintf('mysql: host=%s; dbname=%s; charset=utf8', $db['host'], $db['dbname']);
  
-/* データベース接続 */
 try{
-  /* データベース接続 */
   $pdo = new PDO($dsn, $db['user'], $db['pass']);
-  /* エラーモード設定 */
+
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
 }catch(PDOException $Exception){
-  /* エラーメッセージ */
+
   die('接続エラー:'.$Exception->getMessage());
 }
 ?>
