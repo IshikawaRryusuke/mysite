f1e05eea6ff1dc3b8c9e5acca9588c4dd7a23659
yay <50456775+yayIiTs@users.noreply.github.com>
2019-10-18 14:30:47 +0900

    Add files via upload

---
+++ wordpress化/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>美容室Sakura</title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <div class="top-wrapper">
+      <div class="title">
+        <h2>Sakura Island</h2>
+        <p>予約はグーグルカレンダーに書き込んでください</p>
+      </div>
+      <div id="use" class="yoyaku">
+        <a href="#">予約はこちら</a>
+      </div>
+    </div>
+    <div class="dai">
+      <h2>googleカレンダーの使い方</h2>
+    </div>
+    </div>
+  </body>
+</html>
